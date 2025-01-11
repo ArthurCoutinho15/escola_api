69cbe6313e93cd89c0a3d5baaa3ea3d8c43438f9
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2025-01-10 22:12:01 -0300

    tests: adiciona testes de autenticação e rotas

---
+++ escola/tests/test_authentication.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from django.contrib.auth import authenticate
+from django.urls import reverse
+from rest_framework import status
+
+class AuthenticantionUserTestCase(APITestCase):
+    def setUp(self):
+        self.usuario = User.objects.create_superuser(username='admin', password='admin')
+        self.url = reverse('Estudantes-list')
+    
+    def test_autenticacao_user_credenciais_corretas(self):
+        """
+        Teste que verifica a autenticação de um user com as credenciais corretas
+        """
+        
+        usuario = authenticate(username='admin', password='admin')
+        self.assertTrue((usuario is not None) and usuario.is_authenticated)
+    
+    def test_autenticacao_credenciais_com_username_incorreto(self):
+        """
+        Teste que verifica a autenticação com username incorreto
+        """
+        
+        usuario = authenticate(username='admi', password='admin')
+        self.assertFalse((usuario is not None) and usuario.is_authenticated)
+    
+    def test_autenticacao_credenciais_com_password_incorreto(self):
+        """
+        Teste que verifica a autenticação com password incorreto
+        """
+        
+        usuario = authenticate(username='admin', password='admi')
+        self.assertFalse((usuario is not None) and usuario.is_authenticated)
+    
+    def test_requisicao_get_autorizada(self):
+        """
+        Teste que verifica uma requisição GET autorizada
+        """
+        self.client.force_authenticate(self.usuario)
+        response  = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_requisicao_get_nao_autorizada(self):
+        """
+        Teste que verifica uma requisição GET não autorizada
+        """
+        response  = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
